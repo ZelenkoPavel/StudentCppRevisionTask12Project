e8d2deb174e356af73b9d625f7a6ed9cce430cc6
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-02 10:49:05 +0300

    Add project files.

---
+++ StudentCppRevisionTask12Project/main.cpp
@@ -0,0 +1,14 @@
+#include "tests.h"
+
+int main(void) {
+
+	testingTask01();
+	testingTask02();
+	testingTask03();
+	testingTask04();
+	testingTaskX();
+
+	cout << task01(28, 21) << endl;
+
+	return 0;
+}
---
+++ StudentCppRevisionTask12Project/taskX.cpp
@@ -0,0 +1,32 @@
+#include "tasks.h"
+
+/*	Task X. Non-Unique Number Digits [неуникальные цифры числа]
+*
+*	Дано целое число. Необходимо определить,
+*	есть ли в числе повторяющиеся цифры.
+*
+*	Формат входных данных [input]
+*	На вход функция принимает целое число типа long long.
+*
+*	Формат выходных данных [output]
+*	Функция должна возвращатить булевское значение - результат решения задания.
+*
+*	[ input 1]: 54321
+*	[output 1]: false
+*
+*	[ input 2]: 11111
+*	[output 2]: true
+*
+*	[ input 3]: 121
+*	[output 3]: true
+*
+*	[ input 4]: -12345
+*	[output 4]: false
+*
+*	[ input 5]: 0
+*	[output 5]: false
+*/
+
+bool taskX(long long number) {
+	return false;
+}
